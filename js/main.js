83e8b5eedab56dbefabd1ad9801ef280b3654d9f
honor <deniskorotkin7088@gmail.com>
2026-03-13 10:58:30 +0700

    filter

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 let app = new Vue({
     el: '#app',
     data: {
-        message: 'Приложение запущено'
+        cards: [
+            { id: 1, title: 'Задача 1', column: 1 },
+            { id: 2, title: 'Задача 2', column: 2 },
+            { id: 3, title: 'Задача 3', column: 3 }
+        ]
     },
-    mounted() {
-        console.log(this.message);
+    computed: {
+        column1() {
+            return this.cards.filter(c => c.column === 1);
+        },
+        column2() {
+            return this.cards.filter(c => c.column === 2);
+        },
+        column3() {
+            return this.cards.filter(c => c.column === 3);
+        }
     }
 });
