900083003b1ce9d610b51a9b5bb241adce7abc07
honor <deniskorotkin7088@gmail.com>
2026-03-13 11:24:33 +0700

    filter new

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 let app = new Vue({
     el: '#app',
     data: {
-        message: 'Kanban запущена'
+        cards: [
+            { 
+                id: 1, 
+                title: 'Разработка макета', 
+                description: 'Создать дизайн главной страницы',
+                deadline: '2026-03-20',
+                column: 1,
+                createdAt: '2026-03-13',
+                editedAt: null
+            }
+        ]
     },
-    mounted() {
-        console.log(this.message);
+    computed: {
+        column1() {
+            return this.cards.filter(c => c.column === 1);
+        },
+        column2() {
+            return this.cards.filter(c => c.column === 2);
+        },
+        column3() {
+            return this.cards.filter(c => c.column === 3);
+        },
+        column4() {
+            return this.cards.filter(c => c.column === 4);
+        }
     }
 });
