18f56b44b80c86acc4d2e358a4fe68c4b3ade0f7
honor <deniskorotkin7088@gmail.com>
2026-03-13 11:22:03 +0700

    base

--- js/main.js
+++ js/main.js
@@ -1,21 +1,9 @@
 let app = new Vue({
     el: '#app',
     data: {
-        cards: [
-            { id: 1, title: 'Задача 1', column: 1 },
-            { id: 2, title: 'Задача 2', column: 2 },
-            { id: 3, title: 'Задача 3', column: 3 }
-        ]
+        message: 'Kanban запущена'
     },
-    computed: {
-        column1() {
-            return this.cards.filter(c => c.column === 1);
-        },
-        column2() {
-            return this.cards.filter(c => c.column === 2);
-        },
-        column3() {
-            return this.cards.filter(c => c.column === 3);
-        }
+    mounted() {
+        console.log(this.message);
     }
 });
